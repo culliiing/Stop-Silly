ae974c96f05bd2980b5b9449d508463d4265832f
Chris <103900975+culliiing@users.noreply.github.com>
2023-11-10 20:50:02 +0100

    Walking animation

--- Stop Silly/Assets/Scenes/Scripts/PlayerController.cs
+++ Stop Silly/Assets/Scenes/Scripts/PlayerController.cs
@@ -7,28 +7,48 @@ using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour
 {
     CharacterController controller;
+    Animator animator;
     [SerializeField] float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get the direction of movement
+        // Get the direction of movement, if any
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical);
 
-        // Rotate the player to face the direction of movement
-        Vector3 lookDirection = direction + gameObject.transform.position;
-        gameObject.transform.LookAt(lookDirection);
+        animator.SetBool("IsMoving", IsMoving(direction));
 
         // Move the player
+        Move(direction);
+    }
+
+    void Move(Vector3 direction)
+    {
+        FaceDirection(direction);
         transform.position += direction * speed * Time.deltaTime;
         //controller.Move(direction * speed * Time.deltaTime);
+    }
+
+    void FaceDirection(Vector3 moveDirection)
+    {
+        Vector3 lookDirection = moveDirection + gameObject.transform.position;
+        gameObject.transform.LookAt(lookDirection);
+    }
+
+    bool IsMoving(Vector3 direction)
+    {
+        // If direction is not zero, then we are moving
+        if (direction != Vector3.zero)
+            return true;
+
+        return false;
     }
 }
